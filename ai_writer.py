454ab796e172567578699b2f05b2c358dd3a4b22
AnalyzingBasketball <analyzingbasketball@gmail.com>
2026-01-18 10:47:48 +0100

    Update ai_writer.py

--- ai_writer.py
+++ ai_writer.py
@@ -11,7 +11,7 @@ import numpy as np
 MODEL_NAME = "gemini-2.5-flash"
 FILE_PATH = "data/BoxScore_ACB_2025_Cumulative.csv"
 
-# Mapa de equipos
+# Mapa de Equipos (Para que Gemini diga "Unicaja" en vez de "UNI")
 TEAM_MAP = {
     'UNI': 'Unicaja', 'SBB': 'Bilbao Basket', 'BUR': 'San Pablo Burgos', 'GIR': 'Bàsquet Girona',
     'TEN': 'La Laguna Tenerife', 'MAN': 'BAXI Manresa', 'LLE': 'Hiopos Lleida', 'BRE': 'Río Breogán',
@@ -20,23 +20,327 @@ TEAM_MAP = {
     'VBC': 'Valencia Basket', 'BAR': 'Barça'
 }
 
-# --- LA LISTA DE LA VERDAD ---
-# Si un nombre te da problemas en el futuro, añádelo aquí.
-# Esto corrige el dato ANTES de que la IA lo toque.
+# ==============================================================================
+# 2. DICCIONARIO MAESTRO DE JUGADORES (Temp. 2025/26)
+# ==============================================================================
+# Corrección automática de nombres antes de llamar a la IA.
 CORRECCIONES_VIP = {
-    "F. Alonso": "Francis Alonso",
-    "D. Brankovic": "Danko Brankovic",
-    "M. Normantas": "Margiris Normantas",
-    "A. Best": "Aaron Best",
-    "C. Hunt": "Cameron Hunt",
+    # --- BARÇA (BAR) ---
+    "D. Brizuela": "Darío Brizuela",
+    "D. González": "Dani González",
+    "J. Marcos": "Juani Marcos",
+    "J. Parra": "Joel Parra",
+    "J. Vesely": "Jan Vesely",
+    "K. Punter": "Kevin Punter",
+    "M. Cale": "Myles Cale",
+    "M. Norris": "Miles Norris",
+    "N. Kusturica": "Nikola Kusturica",
+    "N. Laprovittola": "Nico Laprovittola",
+    "S. Keita": "Sayon Keita",
+    "T. Satoransky": "Tomas Satoransky",
+    "T. Shengelia": "Toko Shengelia",
+    "W. Clyburn": "Will Clyburn",
+    "W. Hernangómez": "Willy Hernangómez",
+    "Y. Fall": "Youssoupha Fall",
+
+    # --- BASKONIA (BKN) ---
+    "C. Frisch": "Clément Frisch",
+    "E. Omoruyi": "Eugene Omoruyi",
+    "G. Radzevicius": "Gytis Radzevicius",
+    "H. Diallo": "Hamidou Diallo",
+    "K. Diop": "Khalifa Diop",
+    "K. Simmons": "Kobi Simmons",
+    "L. Samanic": "Luka Samanic",
+    "Luwawu-Cabarrot": "Timothé Luwawu-Cabarrot",
+    "M. Diakite": "Mamadi Diakite",
+    "M. Howard": "Markus Howard",
+    "M. Nowell": "Markquis Nowell",
+    "M. Spagnolo": "Matteo Spagnolo",
+    "R. Kurucs": "Rodions Kurucs",
+    "R. Villar": "Rafa Villar",
+    "S. Joksimovic": "Stefan Joksimovic",
     "T. Forrest": "Trent Forrest",
+    "T. Sedekerskis": "Tadas Sedekerskis",
+
+    # --- RÍO BREOGÁN (BRE) ---
+    "A. Aranitovic": "Aleksandar Aranitovic",
+    "A. Kurucs": "Arturs Kurucs",
+    "B. Dibba": "Bamba Dibba",
+    "D. Apic": "Dragan Apic",
+    "D. Brankovic": "Danko Brankovic",
+    "D. Drežnjak": "Dario Drežnjak",
+    "D. Mavra": "Dominik Mavra",
+    "D. Russell": "Daron Russell",
+    "E. Quintela": "Erik Quintela",
+    "F. Alonso": "Francis Alonso",
+    "J. Sakho": "Jordan Sakho",
+    "K. Cook": "Keaton Cook",
+    "M. Andric": "Mihajlo Andric",
+
+    # --- SAN PABLO BURGOS (BUR) ---
+    "D. Díez": "Dani Díez",
     "E. Happ": "Ethan Happ",
+    "G. Corbalán": "Gonzalo Corbalán",
+    "J. Gudmundsson": "Jon Axel Gudmundsson",
+    "J. Jackson": "Justin Jackson",
+    "J. Rubio": "Joan Rubio",
+    "J. Samuels": "Jermaine Samuels",
+    "L. Fischer": "Luke Fischer",
+    "L. Meindl": "Leo Meindl",
+    "P. Almazán": "Pablo Almazán",
+    "R. Neto": "Raulzinho Neto",
+    "R. Rodríguez": "Rodrigo Rodríguez",
+    "S. García": "Sergi García",
+    "S. de Sousa": "Silvio de Sousa",
+    "Y. Nzosa": "Yannick Nzosa",
+
+    # --- CASADEMONT ZARAGOZA (CAZ) ---
+    "B. Dubljevic": "Bojan Dubljevic",
+    "C. Alías": "Carlos Alías",
+    "C. Koumadje": "Christ Koumadje",
+    "D. Robinson": "Devin Robinson",
+    "D. Stephens": "D.J. Stephens",
+    "E. Kabaca": "Emir Kabaca",
+    "E. Stevenson": "Erik Stevenson",
+    "J. Fernández": "Jaime Fernández",
+    "J. Rodríguez": "Joaquín Rodríguez",
+    "J. Soriano": "Joel Soriano",
+    "L. Langarita": "Lucas Langarita",
+    "M. González": "Miguel González",
+    "M. Lukic": "Matija Lukic",
+    "M. Spissu": "Marco Spissu",
+    "S. Yusta": "Santi Yusta",
+    "T. Bell-Haynes": "Trae Bell-Haynes",
+    "Y. Traoré": "Youssouf Traoré",
+
+    # --- COVIRÁN GRANADA (COV) ---
+    "A. Alibegovic": "Amar Alibegovic",
+    "A. Brimah": "Amida Brimah",
+    "B. Burjanadze": "Beka Burjanadze",
+    "B. Olumuyiwa": "Babatunde Olumuyiwa",
+    "E. Durán": "Edu Durán",
+    "E. Valtonen": "Elias Valtonen",
+    "I. Aurrecoechea": "Iván Aurrecoechea",
+    "J. Kljajic": "Jovan Kljajic",
+    "J. Pérez": "Josep Pérez",
+    "J. Rousselle": "Jonathan Rousselle",
+    "L. Bozic": "Luka Bozic",
+    "L. Costa": "Lluís Costa",
+    "M. Ngouama": "Mehdy Ngouama",
+    "M. Speight": "Micah Speight",
+    "M. Thomas": "Malcolm Thomas",
+    "O. Cerdá": "Osi Cerdá",
+    "P. Tomàs": "Pere Tomàs",
+    "T. Munnings": "Travis Munnings",
+    "W. Howard": "William Howard",
+    "Z. Hankins": "Zach Hankins",
+
+    # --- DREAMLAND GRAN CANARIA (GCA) ---
+    "A. Albicy": "Andrew Albicy",
+    "B. Angola": "Braian Angola",
+    "C. Alocén": "Carlos Alocén",
+    "E. Vila": "Eric Vila",
+    "I. Wong": "Isaiah Wong",
+    "K. Kuath": "Kur Kuath",
+    "L. Labeyrie": "Louis Labeyrie",
+    "L. Maniema": "Lucas Maniema",
+    "M. Salvó": "Miquel Salvó",
+    "M. Tobey": "Mike Tobey",
+    "N. Brussino": "Nico Brussino",
+    "P. Pelos": "Pierre Pelos",
+    "Z. Samar": "Ziga Samar",
+
+    # --- BÀSQUET GIRONA (GIR) ---
+    "D. Needham": "Derek Needham",
+    "G. Ferrando": "Guillem Ferrando",
+    "J. Fernández": "Juan Fernández",
+    "M. Fjellerup": "Máximo Fjellerup",
+    "M. Geben": "Martin Geben",
+    "M. Hughes": "Michael Hughes",
+    "M. Susinskas": "Mindaugas Susinskas",
+    "N. Maric": "Nikola Maric",
+    "O. Livingston": "Otis Livingston",
+    "P. Busquets": "Pep Busquets",
+    "P. Vildoza": "Pato Vildoza",
+    "S. Hollanders": "Sander Hollanders",
+    "S. Martínez": "Sergi Martínez",
+
+    # --- JOVENTUT BADALONA (JOV) ---
+    "A. Hanga": "Adam Hanga",
     "A. Tomic": "Ante Tomic",
-    "G. Corbalán": "Gonzalo Corbalán"
+    "A. Torres": "Adrià Torres",
+    "C. Hunt": "Cameron Hunt",
+    "F. Mauri": "Ferran Mauri",
+    "G. Vives": "Guillem Vives",
+    "H. Drell": "Henri Drell",
+    "L. Hakanson": "Ludde Hakanson",
+    "M. Allen": "Miguel Allen",
+    "M. Ruzic": "Michael Ruzic",
+    "R. Rubio": "Ricky Rubio",
+    "S. Birgander": "Simon Birgander",
+    "S. Dekker": "Sam Dekker",
+    "Y. Kraag": "Yannick Kraag",
+
+    # --- HIOPOS LLEIDA (LLE) ---
+    "A. Diagne": "Amadou Diagne", 
+    "C. Agada": "Caleb Agada",
+    "C. Krutwig": "Cameron Krutwig",
+    "C. Walden": "Corey Walden",
+    "G. Golomán": "Gyorgy Golomán",
+    "I. Dabo": "Ibrahim Dabo",
+    "J. Batemon": "James Batemon",
+    "J. Shurna": "John Shurna",
+    "K. Zoriks": "Kristers Zoriks",
+    "M. Ejim": "Melvin Ejim",
+    "M. Jiménez": "Millán Jiménez",
+    "M. Sanz": "Miquel Sanz",
+    "O. Paulí": "Oriol Paulí",
+    "P. Rios": "Pau Rios",
+
+    # --- BAXI MANRESA (MAN) ---
+    "A. Izaw-Bolavie": "Alexandre Izaw-Bolavie",
+    "A. Plummer": "Alfonso Plummer",
+    "A. Reyes": "Álex Reyes",
+    "A. Ubal": "Agustín Ubal",
+    "D. Pérez": "Dani Pérez",
+    "E. Brooks": "Emanuel Brooks",
+    "F. Bassas": "Ferran Bassas",
+    "F. Torreblanca": "Ferran Torreblanca",
+    "G. Fernández": "Guillem Fernández",
+    "G. Golden": "Grant Golden",
+    "G. Knudsen": "Gustav Knudsen",
+    "H. Benitez": "Hugo Benítez",
+    "K. Akobundu": "Kaodirichi Akobundu",
+    "L. Olinde": "Louis Olinde",
+    "M. Gaspà": "Marc Gaspà",
+    "M. Steinbergs": "Marcis Steinbergs",
+    "P. Oriola": "Pierre Oriola",
+    "R. Obasohan": "Retin Obasohan",
+
+    # --- MORABANC ANDORRA (MBA) ---
+    "A. Best": "Aaron Best",
+    "A. Ganal": "Aaron Ganal",
+    "A. Pustovyi": "Artem Pustovyi",
+    "C. Ortega": "Chumi Ortega",
+    "F. Bassas": "Ferran Bassas",
+    "J. McKoy": "Jordy McKoy",
+    "K. Kostadinov": "Konstantin Kostadinov",
+    "K. Kuric": "Kyle Kuric",
+    "M. Udeze": "Morris Udeze",
+    "R. Guerrero": "Rubén Guerrero",
+    "R. Luz": "Rafa Luz",
+    "S. Evans": "Shannon Evans",
+    "S. Okoye": "Stan Okoye",
+    "X. Castañeda": "Xavier Castañeda",
+    "Y. Pons": "Yves Pons",
+
+    # --- REAL MADRID (RMB) ---
+    "A. Abalde": "Alberto Abalde",
+    "A. Feliz": "Andrés Feliz",
+    "A. Len": "Alex Len",
+    "B. Fernando": "Bruno Fernando",
+    "C. Okeke": "Chuma Okeke",
+    "D. Kramer": "David Kramer",
+    "F. Campazzo": "Facundo Campazzo",
+    "G. Deck": "Gabriel Deck",
+    "G. Grinvalds": "Gunars Grinvalds",
+    "G. Procida": "Gabriele Procida",
+    "I. Almansa": "Izan Almansa",
+    "M. Hezonja": "Mario Hezonja",
+    "S. Llull": "Sergio Llull",
+    "T. Lyles": "Trey Lyles",
+    "T. Maledon": "Théo Maledon",
+    "U. Garuba": "Usman Garuba",
+    "W. Tavares": "Edy Tavares",
+
+    # --- BILBAO BASKET (SBB) ---
+    "A. Font": "Aleix Font",
+    "A. Sylla": "Amar Sylla",
+    "A. Zecevic": "Allan Zecevic",
+    "B. Bagayoko": "Bassala Bagayoko",
+    "B. Errasti": "Beñat Errasti",
+    "D. Hilliard": "Darrun Hilliard",
+    "H. Frey": "Harald Frey",
+    "J. Jaworski": "Justin Jaworski",
+    "L. Petrasek": "Luke Petrasek",
+    "M. Krampelj": "Martin Krampelj",
+    "M. Normantas": "Margiris Normantas",
+    "M. Pantzar": "Melwin Pantzar",
+    "S. Lazarevic": "Stefan Lazarevic",
+    "T. Hlinason": "Tryggvi Hlinason",
+
+    # --- LA LAGUNA TENERIFE (TEN) ---
+    "A. Doornekamp": "Aaron Doornekamp",
+    "B. Fitipaldo": "Bruno Fitipaldo",
+    "D. Bordón": "Diego Bordón",
+    "F. Guerra": "Fran Guerra",
+    "G. Shermadini": "Giorgi Shermadini",
+    "H. Alderete": "Hector Alderete",
+    "J. Fernández": "Jaime Fernández",
+    "J. Sastre": "Joan Sastre",
+    "K. Kostadinov": "Konstantin Kostadinov",
+    "L. Costa": "Lluís Costa",
+    "M. Huertas": "Marcelinho Huertas",
+    "R. Giedraitis": "Rokas Giedraitis",
+    "T. Abromaitis": "Tim Abromaitis",
+    "T. Scrubb": "Thomas Scrubb",
+    "W. Van Beck": "Wesley Van Beck",
+
+    # --- UCAM MURCIA (UCM) ---
+    "D. Cacok": "Devontae Cacok",
+    "D. DeJulius": "David DeJulius",
+    "D. Ennis": "Dylan Ennis",
+    "D. García": "Dani García",
+    "E. Cate": "Emanuel Cate",
+    "H. Sant-Roos": "Howard Sant-Roos",
+    "J. Radebaugh": "Jonah Radebaugh",
+    "M. Diagné": "Moussa Diagné",
+    "M. Forrest": "Michael Forrest",
+    "R. López": "Rubén López de la Torre",
+    "S. Raieste": "Sander Raieste",
+    "T. Nakic": "Toni Nakic",
+    "W. Falk": "Wilhelm Falk",
+    "Z. Hicks": "Zach Hicks",
+
+    # --- UNICAJA (UNI) ---
+    "A. Butajevas": "Arturas Butajevas",
+    "A. Díaz": "Alberto Díaz",
+    "A. Rubit": "Augustine Rubit",
+    "C. Audige": "Chase Audige",
+    "C. Duarte": "Chris Duarte",
+    "D. Kravish": "David Kravish",
+    "E. Sulejmanovic": "Emir Sulejmanovic",
+    "J. Barreiro": "Jonathan Barreiro",
+    "J. Webb": "James Webb III",
+    "K. Perry": "Kendrick Perry",
+    "K. Tillie": "Killian Tillie",
+    "N. Djedovic": "Nihad Djedovic",
+    "O. Balcerowski": "Olek Balcerowski",
+    "T. Kalinoski": "Tyler Kalinoski",
+    "T. Pérez": "Tyson Pérez",
+    "X. Castañeda": "Xavier Castañeda",
+
+    # --- VALENCIA BASKET (VBC) ---
+    "B. Badio": "Brancou Badio",
+    "B. Key": "Braxton Key",
+    "D. Thompson": "Darius Thompson",
+    "I. Iroegbu": "Ike Iroegbu",
+    "I. Nogués": "Isaac Nogués",
+    "J. Montero": "Jean Montero",
+    "J. Pradilla": "Jaime Pradilla",
+    "J. Puerto": "Josep Puerto",
+    "K. Taylor": "Kameron Taylor",
+    "López-Arostegui": "Xabi López-Arostegui",
+    "M. Costello": "Matt Costello",
+    "N. Reuvers": "Nathan Reuvers",
+    "N. Sako": "Neal Sako",
+    "O. Moore": "Omari Moore",
+    "S. de Larrea": "Sergio de Larrea",
+    "Y. Sima": "Yankuba Sima",
 }
 
 # ==============================================================================
-# 2. FUNCIONES AUXILIARES
+# 3. FUNCIONES AUXILIARES
 # ==============================================================================
 def guardar_salida(mensaje, nombre_archivo="newsletter_borrador.md"):
     print(mensaje)
@@ -64,11 +368,14 @@ def extraer_numero_jornada(texto):
     return int(match.group()) if match else 0
 
 def clean_name(name_raw):
-    # Si está en la lista VIP, lo corrige. Si no, devuelve el original (J. Smith)
+    """
+    Recibe 'F. Alonso' y devuelve 'Francis Alonso' si está en la lista.
+    Si no está (caso raro), devuelve el original.
+    """
     return CORRECCIONES_VIP.get(name_raw, name_raw)
 
 # ==============================================================================
-# 3. CARGA DE DATOS
+# 4. CARGA DE DATOS
 # ==============================================================================
 api_key = os.environ.get("GEMINI_API_KEY")
 if not api_key: guardar_salida("❌ Error: Falta GEMINI_API_KEY.")
@@ -89,7 +396,7 @@ df_week = df[df['Week'] == ultima_jornada_label]
 print(f"🤖 Analizando {ultima_jornada_label}...")
 
 # ==============================================================================
-# 4. PREPARACIÓN DE DATOS (CON CORRECCIÓN MANUAL SEGURA)
+# 5. PREPARACIÓN DE DATOS (CON LIMPIEZA AUTOMÁTICA)
 # ==============================================================================
 
 # A. MVP
@@ -97,8 +404,10 @@ ganadores = df_week[df_week['Win'] == 1]
 pool = ganadores if not ganadores.empty else df_week
 mvp = pool.sort_values('VAL', ascending=False).iloc[0]
 
-# Aplicamos corrección VIP aquí mismo
-mvp_name = clean_name(mvp['Name'])
+# --- AQUÍ OCURRE LA MAGIA ---
+mvp_name = clean_name(mvp['Name']) 
+# ----------------------------
+
 txt_mvp = (f"{mvp_name} ({get_team_name(mvp['Team'])}): {b(mvp['VAL'])} VAL, "
            f"{b(mvp['PTS'])} PTS (TS%: {b(mvp['TS%'], 1, True)}), {b(mvp['Reb_T'])} REB.")
 
@@ -107,7 +416,7 @@ resto = df_week[df_week['PlayerID'] != mvp['PlayerID']]
 top_rest = resto.sort_values('VAL', ascending=False).head(3)
 txt_rest = ""
 for _, row in top_rest.iterrows():
-    # Aplicamos corrección VIP
+    # --- Limpieza por fila ---
     r_name = clean_name(row['Name'])
     txt_rest += f"- {r_name} ({get_team_name(row['Team'])}): {b(row['VAL'])} VAL.\n"
 
@@ -132,7 +441,7 @@ txt_teams = f"""
 # D. CONTEXTO
 lider_ts = df_week[df_week['PTS'] >= 10].sort_values('TS%', ascending=False).iloc[0]
 lider_usg = df_week.sort_values('USG%', ascending=False).iloc[0]
-# Corrección VIP
+# --- Limpieza de líderes ---
 ts_name = clean_name(lider_ts['Name'])
 usg_name = clean_name(lider_usg['Name'])
 
@@ -149,19 +458,19 @@ if len(jornadas_unicas) >= 1:
     means = df_last.groupby(['Name', 'Team'])[['VAL', 'PTS', 'TS%']].mean().reset_index()
     hot = means.sort_values('VAL', ascending=False).head(5)
     for _, row in hot.iterrows():
-        # Corrección VIP
+        # --- Limpieza de tendencias ---
         t_name = clean_name(row['Name'])
         txt_trends += (f"- {t_name} ({get_team_name(row['Team'], False)}): "
                        f"{b(row['VAL'], 1)} VAL, {b(row['PTS'], 1)} PTS.\n")
 
 # ==============================================================================
-# 5. GENERACIÓN IA (SIN HERRAMIENTAS, SOLO TEXTO)
+# 6. GENERACIÓN IA (TEXTO PURO, SIN HERRAMIENTAS, 100% FIABLE)
 # ==============================================================================
 
 prompt = f"""
-Actúa como Periodista Deportivo ACB (Liga Endesa).
+Actúa como Periodista Deportivo experto en la Liga Endesa (ACB).
 
-DATOS DE LA JORNADA (Ya procesados):
+DATOS DE LA JORNADA (Nombres ya verificados):
 MVP: {txt_mvp}
 DESTACADOS:
 {txt_rest}
@@ -173,9 +482,9 @@ TENDENCIAS:
 {txt_trends}
 
 INSTRUCCIONES:
-1. **NO INVENTES NOMBRES**: Usa los nombres EXACTAMENTE como aparecen en los datos de arriba. Si pone "J. Smith", escribe "J. Smith". Si pone "Francis Alonso", escribe "Francis Alonso".
-2. **NO INVENTES POSICIONES**: Si no sabes si es base o pívot, usa "jugador" o "referente".
-3. **ESTILO**: Periodístico, densa en datos, profesional.
+1. **RESPETA LOS NOMBRES**: Úsalos tal cual aparecen en los datos de arriba (ya están corregidos: ej: "Francis Alonso").
+2. **NARRATIVA**: Escribe una crónica vibrante y densa en datos.
+3. **POSICIONES**: Si no estás seguro de la posición de un jugador, usa términos genéricos como "la figura", "el referente", "el exterior/interior".
 
 ESTRUCTURA DE SALIDA:
 ## 🏀 Informe ACB: {ultima_jornada_label}
@@ -184,7 +493,7 @@ ESTRUCTURA DE SALIDA:
 [Análisis del MVP]
 
 ### 🚀 Radar de Eficiencia
-[Análisis de destacados]
+[Análisis de destacados y contexto]
 
 ### 🧠 Pizarra Táctica
 [Análisis de equipos]
@@ -194,18 +503,10 @@ ESTRUCTURA DE SALIDA:
 """
 
 try:
-    print("🚀 Generando crónica (Modo Seguro - Sin Búsqueda)...")
-    
-    # SIN TOOLS. ESTO NO PUEDE FALLAR.
+    print("🚀 Generando crónica (Modo Infalible)...")
     model = genai.GenerativeModel(MODEL_NAME)
-    
     response = model.generate_content(prompt)
-    
-    if response.text:
-        texto = response.text.replace(":\n-", ":\n\n-")
-        guardar_salida(texto)
-    else:
-        print("❌ Error: Respuesta vacía.")
-
+    texto = response.text.replace(":\n-", ":\n\n-")
+    guardar_salida(texto)
 except Exception as e:
     guardar_salida(f"❌ Error Gemini: {e}")
